6d6a174c44066b3700a8242277fceab8e4d507c0
Sungil Saul Lee <jangsoo86@gmail.com>
2019-06-19 02:23:42 +0000

    Game Object Image added

--- game.js
+++ game.js
@@ -1,3 +1,8 @@
-window.onload = function() {
-    var game = new Phaser.Game();
-}
+var config = {
+    width: 256,
+    height: 272,
+    backgroundColor: 0x000000,
+    scene: [Scene1, Scene2]
+};
+
+var game = new Phaser.Game(config);
